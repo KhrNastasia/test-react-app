aa40d9fed035a89daa41d3a2174023eb221800ce
Stas Konashuk <78376003+StasKonashuk@users.noreply.github.com>
2024-12-11 21:45:39 +0300

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,51 +1,53 @@
-import { useState } from 'react'
-import ListForm from '../ListForm/ListForm'
-import ListItem from '../ListItem/ListItem'
-import classes from './List.module.css'
+import { useState } from 'react';
+import ListForm from '../ListForm/ListForm';
+import ListItem from '../ListItem/ListItem';
+import classes from './List.module.css';
 
 function List() {
-	const [num, setNum] = useState('')
+  const [num, setNum] = useState('');
 
-	const [arr, setArr] = useState([])
+  const [arr, setArr] = useState([]);
 
-	function handleChange(event) {
-		setNum(event.target.value)
-	}
+  function handleChange(event) {
+    setNum(event.target.value);
+  }
 
-	function handleSubmit(e) {
-		e.preventDefault()
-		if (num) {
-			setArr(arr => [...arr, num])
-			setNum('')
-		}
-	}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (num) {
+      setArr((arr) => [...arr, num]);
+      setNum('');
+    }
+  }
 
-	function del(item) {
-		// setArr(arr=>arr.shift())
-		// const i = arr.indexOf(event.target.id)
-		console.log(item)
-		// arr.splice(i, 1)
-		// setArr(arr => [...arr])
+  const removeItemHandler = (item) => {
+    //     setArr(arr=> arr.shift())
+    //     const i = arr.indexOf(event.target.id)
+    console.log(item);
+    //     arr.splice(i, 1)
+    //     setArr(arr => [...arr])
 
-		// setArr(
-		// 	arr.filter(a => {
-		// 		// a.key !== arr.key
-		// 		console.log(a)
-		// 		console.log(arr)
-		// 	})
-		// )
-	}
+    //     setArr(
+    //     	arr.filter(a => {
+    //     		// a.key !== arr.key
+    //     		console.log(a)
+    //     		console.log(arr)
+    //     	})
+    //     )
+  };
 
-	return (
-		<div className={classes.div}>
-			<ul>
-				{arr.map((item, key) => {
-					return <ListItem item={item} key={key} onClick={del} />
-				})}
-			</ul>
-			<ListForm num={num} onChange={handleChange} onSubmit={handleSubmit} />
-		</div>
-	)
+  return (
+    <div className={classes.div}>
+      <ul>
+        {arr.map((item, key) => {
+          return (
+            <ListItem item={item} key={key} onRemove={removeItemHandler} />
+          );
+        })}
+      </ul>
+      <ListForm num={num} onChange={handleChange} onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
-export default List
+export default List;
