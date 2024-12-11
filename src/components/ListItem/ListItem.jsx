bf45ecb1e3995627ee4b957ec34d90073d19f51e
Stas Konashuk <78376003+StasKonashuk@users.noreply.github.com>
2024-12-11 21:46:09 +0300

    Update ListItem.jsx

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,32 +1,28 @@
-import Button from '../Button/Button'
-import classes from './ListItem.module.css'
+import Button from '../Button/Button';
+import classes from './ListItem.module.css';
 
 export default function ListItem(props) {
-	const {item, key, onClick} = props;
+  const { item, onRemove } = props;
 
-	
-	return (
-		<div className={classes.div}>
-			<li className={classes.li}>{item}</li>
-			<div>
-				{/* <Button
-					className={classes.button}
-					id={props.item}
-					onClick={props.onClick}
-				>
-					Редактировать
-				</Button> */}
-				<Button
-					className={classes.button}
-					id={item}
-					onClick={(e) => {
-						e.stopPropagation();
-						onClick(key)
-					}}
-				>
-					Удалить
-				</Button>
-			</div>
-		</div>
-	)
+  return (
+    <div className={classes.div}>
+      <li className={classes.li}>{item}</li>
+      <div>
+        {/* <Button
+		className={classes.button}
+		id={props.item}
+		onClick={props.onClick}
+		>
+		 Редактировать
+		</Button> */}
+        <Button
+          className={classes.button}
+          id={item}
+          onClick={(e) => onRemove(item)}
+        >
+          Удалить
+        </Button>
+      </div>
+    </div>
+  );
 }
